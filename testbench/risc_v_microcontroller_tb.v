c7cc5e89989e214c65495f9de9e7ed5a6d635b0b
amar-singh-wisesciencewise <singha15.extc@coep.ac.in>
2021-05-19 23:29:31 +0530

    trying to read opcode inside risc v cpu; not working

--- testbench/risc_v_microcontroller_tb.v
+++ testbench/risc_v_microcontroller_tb.v
@@ -4,5 +4,19 @@
 
 module test;
 
+reg clk = 0;
+reg reset = 0;
+
+initial begin
+#10 reset = 1;
+#10 reset = 0;
+#50 $finish;
+end //intial end
+
+
+always #10 clk = !clk;
+
+risc_v_microcontroller uc1 (.clk(clk), .reset(reset));
+
 
 endmodule
