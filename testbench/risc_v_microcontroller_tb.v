a3dcd635a89ff1dc83d77b810eaa1d128b0ad456
amar-singh-wisesciencewise <singha15.extc@coep.ac.in>
2021-05-22 13:55:17 +0530

    removing wrong register file and adding risc-v register file code; also modifying testbench which exhibit fetching of opcode

--- testbench/risc_v_microcontroller_tb.v
+++ testbench/risc_v_microcontroller_tb.v
@@ -8,13 +8,15 @@ reg clk = 0;
 reg reset = 0;
 
 initial begin
-#10 reset = 1;
-#10 reset = 0;
-#50 $finish;
+#20 reset = 1;
+#20 reset = 0;
+#20 reset = 1;
+#20 reset = 0;
+#100 $finish;
 end //intial end
 
 
-always #10 clk = !clk;
+always #5 clk = !clk;
 
 risc_v_microcontroller uc1 (.clk(clk), .reset(reset));
 
