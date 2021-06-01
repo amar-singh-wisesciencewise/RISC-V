61e941d43cf8a5d0955e915c6d0d7bcbc620352f
amar-singh-wisesciencewise <singha15.extc@coep.ac.in>
2021-06-01 22:00:32 +0530

    moving defines from decoder to another file called define.v

--- code/decoder.v
+++ code/decoder.v
@@ -1,49 +1,6 @@
 
 `timescale 1ns / 1ps
 
-/*
-`define IS_R_TYPE 0
-`define IS_I_TYPE 1
-`define IS_S_TYPE 2
-`define IS_B_TYPE 3
-`define IS_U_TYPE 4
-`define IS_J_TYPE 5
-*/
-
-`define IS_LUI 6 
-`define IS_AUIPC 7 
-`define IS_JAL 8 
-`define IS_JALR 9
-
-`define IS_BEQ 10 
-`define IS_BNE 11
-`define IS_BLT 12
-`define IS_BGE 13
-`define IS_BLTU 14 
-`define IS_BGEU 15
-
-`define IS_ADDI 16
-`define IS_SLTI 17
-`define IS_SLTIU 18 
-`define IS_XORI 19
-`define IS_ORI 20
-`define IS_ANDI 21 
-`define IS_SLLI 22
-`define IS_SRLI 23
-`define IS_SRAI 24
-`define IS_ADD 25
-`define IS_SUB 26
-`define IS_SLL 27
-`define IS_SLT 28
-`define IS_SLTU 29 
-`define IS_XOR 30
-`define IS_SRL 31
-`define IS_SRA 32
-`define IS_OR 33
-`define IS_AND 34 
-`define IS_LOAD 35
-`define IS_STORE 36
-
 `define IS_U_INSTR(x) ((x[6:2] == 5'b01101) || (x[6:2] == 5'b00101))
 `define IS_I_INSTR(x) ((x[6:2] == 5'b00001) || (x[6:2] == 5'b11001) || (x[6:2] == 5'b00000) || (x[6:2] == 5'b00100) || (x[6:2] == 5'b00110))
 
