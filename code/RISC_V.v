9ace6f1c8635f533bb601e6ce3be56cc00b8bf4a
amar-singh-wisesciencewise <singha15.extc@coep.ac.in>
2021-07-18 12:05:52 +0530

    Data Hazards handling

--- code/RISC_V.v
+++ code/RISC_V.v
@@ -19,7 +19,7 @@ input [BUS_WIDTH-1 : 0] data_in;
 output reg iwr = 1'b0;
 output reg wr /* store */, re /* load */;
 
-reg [BUS_WIDTH-1 : 0] pc;
+reg [BUS_WIDTH-1 : 0] pc = 0;
 reg [BUS_WIDTH-1 : 0] pc_fetch;
 reg [BUS_WIDTH-1 : 0] pc_decoder;
 reg [BUS_WIDTH-1 : 0] pc_rf; //pc for register file
@@ -45,7 +45,7 @@ reg [REG_WIDTH-1 : 0] rs2_execute;
 reg [REG_WIDTH-1 : 0] rd_execute; 
 reg [REG_WIDTH-1 : 0] rs1_ls; 
 reg [REG_WIDTH-1 : 0] rs2_ls; 
-reg [REG_WIDTH-1 : 0] rd_ls; 
+reg [REG_WIDTH-1 : 0] rd_ls = 5'b0; 
 reg [REG_WIDTH-1 : 0] rs1_wb; 
 reg [REG_WIDTH-1 : 0] rs2_wb; 
 reg [REG_WIDTH-1 : 0] rd_wb; 
@@ -58,7 +58,7 @@ reg [BUS_WIDTH-1 : 0] imm_wb; //for write back
 reg rde_decoder, rs1e_decoder, rs2e_decoder; //enable signals of rs1 rs2 and rd
 reg rde_rf, rs1e_rf, rs2e_rf;
 reg rde_execute, rs1e_execute, rs2e_execute;
-reg rde_ls, rs1e_ls, rs2e_ls;
+reg rde_ls = 1'b0, rs1e_ls, rs2e_ls;
 reg rde_wb, rs1e_wb, rs2e_wb;
 wire [BUS_WIDTH-1 : 0] imm; //immediate of decoder
 wire rde, rs1e, rs2e;
@@ -81,6 +81,10 @@ reg is_taken_ls;
 
 wire [BUS_WIDTH-1 : 0] rdata_; //for load store module
 
+reg [(1 << REG_WIDTH) - 1 : 0] reg_not_ready = 32'b0;
+reg cpu_stall = 1'b0;
+reg [REG_WIDTH-1:0] not_ready_rs = 0;
+
 decoder dec1(.reset(reset), .inst(inst), .instr_type(instr_type), .rs1(rs1), .rs2(rs2), .rd(rd), .rs1e(rs1e), .rs2e(rs2e), .rde(rde), .imm(imm));
 execute ex1(.reset(reset), .instr_type(instr_type_rf), .pc(pc_rf), .rs1(rs1_data_rf), .rs2(rs2_data_rf), .imm(imm_rf), .result(result), .is_taken(is_taken));
 
@@ -97,19 +101,23 @@ always@(posedge clk) begin
 	//if branch change the PC
 		if (is_taken)
 			pc <= result;
-		else
-			pc <= pc + 4;
+		else begin
+			if (!cpu_stall)
+				pc <= pc + 4;
+		end
 	end // else end
 end //always
 
 //fetch block
 always@(posedge clk) begin
-	pc_fetch <= pc;
-	if (is_taken)
+	//pc_fetch <= (cpu_stall) ? pc_fetch : pc + 4;
+	pc_fetch <= pc + 4;
+	if (is_taken) begin
 		inst <= `NOP_INSTR;
-	else
-		inst <= idata;
-//	$display("%x",inst);
+		reg_not_ready <= 0;
+	end else begin
+		inst <= (cpu_stall) ? inst : idata;
+	end
 end //always end
 
 //decode block
@@ -125,16 +133,40 @@ always@(posedge clk) begin
 		rde_decoder <= 0;
 		imm_decoder <= 0;
 	end else begin
-		pc_decoder <= pc_fetch; //propogate	
-		instr_type_decoder <= (is_taken) ? `IS_ADD : instr_type;
-		rs1_decoder <= (is_taken) ? 0 : rs1;
-		rs2_decoder <= (is_taken) ? 0 : rs2;
-		rd_decoder <= (is_taken) ? 0 : rd;
-		rs1e_decoder <= (is_taken) ? 1 : rs1e;
-		rs2e_decoder <= (is_taken) ? 1 : rs2e;
-		rde_decoder <= (is_taken) ? 1 : rde;
-		imm_decoder <= (is_taken) ? 0 : imm;
-//	$display("instr type %x imm %x rs1 %x rs2 %x rd %x valid %x%x%x", instr_type, imm, rs1, rs2, rd, rs1e, rs2e, rde);
+			pc_decoder <= pc_fetch; //propogate	
+		if (!cpu_stall) begin
+			instr_type_decoder <= (is_taken) ? `IS_ADD : instr_type;
+			rs1_decoder <= (is_taken) ? 0 : rs1;
+			rs2_decoder <= (is_taken) ? 0 : rs2;
+			rd_decoder <= (is_taken) ? 0 : rd;
+			rs1e_decoder <= (is_taken) ? 1 : rs1e;
+			rs2e_decoder <= (is_taken) ? 1 : rs2e;
+			rde_decoder <= (is_taken) ? 1 : rde;
+			imm_decoder <= (is_taken) ? 0 : imm;
+		end //if end
+
+		//set and clear the destination registers in the pipeline
+		if ((!is_taken && rde && rd) || rde_ls)
+			reg_not_ready <= (reg_not_ready | (rde << rd)) & ~(rde_ls << rd_ls);
+
+		// set the CPU Stall and store the register which stalled
+		if (!cpu_stall) begin
+			if (rs1e && (reg_not_ready & (1 << rs1))) begin
+				cpu_stall <= 1;
+				not_ready_rs <= rs1;
+			end else if (rs2e && (reg_not_ready & (1 << rs2))) begin
+				cpu_stall <= 1;
+				not_ready_rs <= rs2;
+			end else begin
+				not_ready_rs <= 0;
+			end
+		end //if end
+
+		//clear stall in case we know that rd_ls will clear the not_ready_rs
+		if (cpu_stall && rde_ls && (rd_ls == not_ready_rs)) begin
+			not_ready_rs <= 0;
+			cpu_stall <= 0;
+		end
 	end //else
 end //always 
 
@@ -153,19 +185,19 @@ always@(posedge clk) begin
 		rs1_data_rf <= 0;
 		rs2_data_rf <= 0;
 	end else begin
-		pc_rf <= pc_decoder; //propogate	
-		instr_type_rf <= (is_taken) ? `IS_ADD : instr_type_decoder;
-		rs1_rf <= (is_taken) ? 0 : rs1_decoder;
-		rs2_rf <= (is_taken) ? 0 : rs2_decoder;
-		rd_rf <= (is_taken) ? 0 : rd_decoder;
-		rs1e_rf <= (is_taken) ? 1 : rs1e_decoder;
-		rs2e_rf <= (is_taken) ? 1 : rs2e_decoder;
-		rde_rf <= (is_taken) ? 1 : rde_decoder;
-		imm_rf <= (is_taken) ? 0 : imm_decoder;
+		pc_rf <= pc_decoder; //propogate
+		instr_type_rf <= (is_taken || cpu_stall) ? `IS_ADD : instr_type_decoder;
+		rs1_rf <= (is_taken || cpu_stall) ? 0 : rs1_decoder;
+		rs2_rf <= (is_taken || cpu_stall) ? 0 : rs2_decoder;
+		rd_rf <= (is_taken || cpu_stall) ? 0 : rd_decoder;
+		rs1e_rf <= (is_taken || cpu_stall) ? 1 : rs1e_decoder;
+		rs2e_rf <= (is_taken || cpu_stall) ? 1 : rs2e_decoder;
+		rde_rf <= (is_taken || cpu_stall) ? 1 : rde_decoder;
+		imm_rf <= (is_taken || cpu_stall) ? 0 : imm_decoder;
 
 		//use decoder values for register file read
-		rs1_data_rf <= (is_taken) ? 0 : rs1_data;
-		rs2_data_rf <= (is_taken) ? 0 : rs2_data;
+		rs1_data_rf <= (is_taken || cpu_stall) ? 0 : rs1_data;
+		rs2_data_rf <= (is_taken || cpu_stall) ? 0 : rs2_data;
 	end //else
 end //always end
 
@@ -215,7 +247,6 @@ always@(posedge clk) begin
 
 		result_execute <= result;
 		is_taken_execute <= is_taken;
-//		$display("result %h",result_execute);
 		
 	end //else
 end //always end
