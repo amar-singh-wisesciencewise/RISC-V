cbe9d347b056c69b79c2855ab6a52baf3a742f4e
amar-singh-wisesciencewise <singha15.extc@coep.ac.in>
2022-03-05 19:53:58 +0530

    Changing the test framework

--- code/compile_script.sh
+++ code/compile_script.sh
@@ -1,5 +1,6 @@
 
 
 #iverilog define.v instruction_memory.v data_mem.v decoder.v execute.v risc_v_rf.v RISC_V.v risc_v_microcontroller.v
-iverilog define.v instruction_memory.v data_mem.v decoder.v execute.v risc_v_rf.v RISC_V.v risc_v_microcontroller.v ../testbench/trf_gen.v ../testbench/risc_v_tb.v
+#iverilog define.v instruction_memory.v data_mem.v decoder.v execute.v risc_v_rf.v RISC_V.v risc_v_microcontroller.v ../testbench/trf_gen.v ../testbench/risc_v_tb.v
 
+iverilog define.v instruction_memory.v data_mem.v decoder.v execute.v risc_v_rf.v RISC_V.v risc_v_microcontroller.v ../testbench/risc_v_microcontroller_tb.v
